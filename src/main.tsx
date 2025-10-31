d072930f7df27bb69aa645ae0e5bccf8b159c5c8
stevelhm-ux <steve.lhm@gmail.com>
2025-10-31 01:17:03 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,19 @@
-// src/main.tsx
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import AuthGate from './AuthGate'
-import './index.css'
-
-const rootEl = document.getElementById('root')!
-createRoot(rootEl).render(
-  <React.StrictMode>
-    <AuthGate>
-      <App />
-    </AuthGate>
-  </React.StrictMode>
-)
-
+// src/main.tsx (bottom part)
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {})
+    navigator.serviceWorker.register('/sw.js').then(reg => {
+      // If there’s an updated worker waiting, reload when it activates
+      if (reg.waiting) reg.waiting.postMessage({ type: 'SKIP_WAITING' })
+      reg.addEventListener('updatefound', () => {
+        const nw = reg.installing
+        if (!nw) return
+        nw.addEventListener('statechange', () => {
+          if (nw.state === 'installed' && navigator.serviceWorker.controller) {
+            // new SW installed and will claim clients; reload to get fresh files
+            location.reload()
+          }
+        })
+      })
+    }).catch(() => {})
   })
 }
