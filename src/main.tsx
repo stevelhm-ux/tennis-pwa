55e999ada79ebe33a6fc8d96f33a591cbe2cb2bb
stevelhm-ux <steve.lhm@gmail.com>
2025-10-31 01:35:29 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
-// src/main.tsx (bottom part)
+// src/main.tsx (bottom)
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(reg => {
-      // If there’s an updated worker waiting, reload when it activates
-      if (reg.waiting) reg.waiting.postMessage({ type: 'SKIP_WAITING' })
-      reg.addEventListener('updatefound', () => {
-        const nw = reg.installing
-        if (!nw) return
-        nw.addEventListener('statechange', () => {
-          if (nw.state === 'installed' && navigator.serviceWorker.controller) {
-            // new SW installed and will claim clients; reload to get fresh files
-            location.reload()
-          }
-        })
-      })
-    }).catch(() => {})
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(r => r.unregister())
   })
+  // TEMP: do NOT register a SW while recovering
+  // window.addEventListener('load', () => {
+  //   navigator.serviceWorker.register('/sw.js').catch(() => {})
+  // })
 }
